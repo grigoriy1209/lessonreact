8deb0d25c5b1c2010c0b0d637b255843c189b583
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-02-03 16:22:15 +0200

    lesson2.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import {useEffect} from "react";
+import {getAllUsers, getUserById} from "./services/user.api.service";
+
+
 const App = () => {
+    useEffect(() => {
+        getAllUsers().then(value => console.log(value.data))
+        getUserById(3).then(value => console.log(value.data))
+    }, []);
     return (
         <div>
-            App
+
         </div>
     );
 };
