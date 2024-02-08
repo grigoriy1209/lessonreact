d56eb028043fe356e3ca3555306223a4136ccf0a
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-02-08 22:38:16 +0200

    lesson2.3

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import {useState} from "react";
 
 const App = () => {
     const [user, setUser] = useState(null)
+
     const choseUser = (obj)=>{
       setUser(obj)
     }
     return (
         <div>
-           <h2>{user?.email}</h2>
-            {
-                user && <h2>{user.email}</h2>
-            }
+
 
             <hr/>
             <h2>user component start</h2>
               <UsersComponent choseUser ={choseUser}/>
             <h2>user component end</h2>
             <hr/>
+            <h2>{user?.email}</h2>
+            {
+                user && <h2>{user.email}</h2>
+            //     це кращий варіант^^^
+            }
         </div>
     );
 };
